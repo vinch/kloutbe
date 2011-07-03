c00eec0942e53e343981db0e6170da6319303f2b
Vincent Battaglia <vincent.battaglia@gmail.com>
2011-07-03 20:42:34 +0200

    Meta description + GA

--- index.php
+++ index.php
@@ -29,11 +29,25 @@ while ($user = mysql_fetch_assoc($query)) {
 <html>
 <head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<title>Belgians on Klout</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="description" content="Ranking of Belgian influencers on Klout">
 	<link rel="stylesheet" type="text/css" media="screen" href="_css/main.css" />
 	<link rel="stylesheet" type="text/css" media="screen and (max-width: 900px)" href="_css/900.css" />
 	<link rel="stylesheet" type="text/css" media="screen and (max-width: 650px)" href="_css/650.css" />
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-192063-4']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 	<!--[if lt IE 9]>
 	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
